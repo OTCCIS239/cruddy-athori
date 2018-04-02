4e8be5fec428664a6e6461cf22d95b0d20e57e30
jstass <jasonstass@gmail.com>
2018-04-02 13:30:12 -0500

    Adding home page

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <p class="myFooterText">Copyright Jason Stass 2018</p>
+      </div>
+    </div>
+  </nav>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.master')
     @section('content')
-
+        @include('layouts.nav')
+        <div class="container homeContainer">
+            <form action="">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">User Name</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        @include('layouts.footer')
     @endsection
