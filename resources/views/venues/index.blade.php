7e0ff41edaecde137dccfba1257b862fabea179a
jstass <jasonstass@gmail.com>
2018-04-02 16:05:57 -0500

    Adding Nav links and fixing css

--- resources/views/venues/index.blade.php
+++ resources/views/venues/index.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.master')
     @section('content')
         @include('layouts.nav')
-        <div class="container usersContainer">
-            <h1>User's Information</h1>
+        <div class="container venuesContainer">
+            <h1>Venue's Information</h1>
             <table class="table">
                 <thead class="thead-dark">
                   <tr>
-                    <th scope="col">User ID</th>
-                    <th scope="col">Username</th>
-                    <th scope="col">First Name</th>
-                    <th scope="col">Last Name</th>
+                    <th scope="col">Venue ID</th>
+                    <th scope="col">Venue Name</th>
+                    <th scope="col">misc info</th>
+                    <th scope="col">misc info2</th>
                   </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <th scope="row">1</th>
-                        <td>ausername</td>
-                        <td>John</td>
-                        <td>Doe</td>
+                        <td>A venue</td>
+                        <td>misc info k</td>
+                        <td>more misc info k</td>
                     </tr>
                 </tbody>
             </table>
