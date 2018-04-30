3333e551c8eea1ed6b5b7ef35dde70f06a383621
jstass <jasonstass@gmail.com>
2018-04-30 06:51:10 -0500

    Adding info to UsersController and User Model

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Users;
+use App\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -14,7 +14,8 @@ class UsersController extends Controller
      */
     public function index()
     {
-        return view('users.index');
+        $users = User::all();
+        return view('users.index', compact('users'));
     }
 
     /**
@@ -35,52 +36,59 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'username'=>'required'
+        ]);
+
+        $user = User::create($request->all());
+
+        return redirect('/users/' . $user->id);
     }
 
     /**
      * Display the specified resource.
-     *
-     * @param  \App\Users  $users
+     * @param int $id
+     * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    // public function show(Users $users)
-    public function show()
+    public function show(User $user)
     {
-        return view('users.show');
+        return view('users.show', compact('user'));
     }
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  \App\Users  $users
+     * @param int $id
+     * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function edit(Users $users)
+    public function edit(User $user)
     {
-        //
+        return view('users.edit', compact('user'));
     }
 
     /**
      * Update the specified resource in storage.
-     *
+     * @param int $id
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Users  $users
+     * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Users $users)
+    public function update(Request $request, User $user)
     {
-        //
+        $user->update($request->all());
+        return redirect('/users/' . $user->id);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Users  $users
+     * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Users $users)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return redirect('/users');
     }
 }
--- app/User.php
+++ app/User.php
@@ -15,7 +15,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username',
+        'name',
+        'type',
+        'email',
+        'usertype',
+        'password'
     ];
 
     /**
